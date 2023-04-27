c8f8e17b7223532fe7532e2b6264c5788be240cf
AlyssonGmes <alysson.gomes63@gmail.com>
2023-04-26 23:38:04 -0300

    Alteração do Main.

--- ArvoreBinaria/src/Main.java
+++ ArvoreBinaria/src/Main.java
@@ -1,23 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        ArvoreBinInteiro n = new ArvoreBinInteiro();
-        n.inserir(10);
-        n.inserir(9);
-        n.inserir(8);
-        n.inserir(5);
-        n.inserir(4);
-        n.inserir(6);
-        n.inserir(2);
-        n.inserir(11);
-        n.inserir(7);
-        n.inserir(12);
-        n.inserir(15);
-        n.inserir(14);
-        n.inserir(17);
-        n.inserir(23);
-        n.inserir(13);
-        //System.out.println(n.retornaMaiorNumero());
-        //n.listarArvore(4, 5);
-        System.out.println(n.retornarAlturaArvore());
+        TesteArvoreBinInteiro.menu();
     }
 }
